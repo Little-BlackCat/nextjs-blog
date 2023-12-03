48f9370b1824376893c12b543231534e2bc530ac
BlackCat <neko.sword@gmail.com>
2023-12-03 16:23:44 +0700

    add: deploy config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/nextjs-blog/' : '',
+};
+
